fc26ce87a58b5d34a0fb7fedb51888dd91bd6e7b
SandeepHooda <sonu.hooda@gmail.com>
2018-03-31 13:26:03 +0530

    sending email

--- src/Welcome.java
+++ src/Welcome.java
@@ -1,19 +1,44 @@
 
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.Base64;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.communication.email.TembooliveVO;
+import com.google.appengine.api.urlfetch.FetchOptions;
+import com.google.appengine.api.urlfetch.HTTPHeader;
+import com.google.appengine.api.urlfetch.HTTPMethod;
+import com.google.appengine.api.urlfetch.HTTPRequest;
+import com.google.appengine.api.urlfetch.URLFetchService;
+import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.mailjet.client.ClientOptions;
+import com.mailjet.client.MailjetClient;
+import com.mailjet.client.MailjetRequest;
+import com.mailjet.client.MailjetResponse;
+import com.mailjet.client.errors.MailjetException;
+import com.mailjet.client.errors.MailjetSocketTimeoutException;
+import com.mailjet.client.resource.Emailv31;
+
 /**
  * Servlet implementation class Welcome
  */
-@WebServlet("/Welcome")
+
 public class Welcome extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+	private static FetchOptions lFetchOptions = FetchOptions.Builder.doNotValidateCertificate().setDeadline(300d);
+	private static URLFetchService fetcher = URLFetchServiceFactory.getURLFetchService();
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -26,7 +51,32 @@ public class Welcome extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.sendRedirect("/MyShop/index.html");
+		
+		/*System.setProperty("http.proxyHost", "127.0.0.1");
+		System.setProperty("https.proxyHost", "127.0.0.1");
+		System.setProperty("http.proxyPort", "8888");
+		System.setProperty("https.proxyPort", "8888");*/
+String httpsURL = "https://api.mailjet.com/v3.1/send";
+		
+		URL url = new URL(httpsURL);
+		 HTTPRequest req = new HTTPRequest(url, HTTPMethod.POST, lFetchOptions);
+        HTTPHeader header = new HTTPHeader("Content-type", "application/json");
+        req.setHeader(header);
+        
+      
+       
+        String encoding = Base64.getEncoder().encodeToString("3b80493548b82c742b0336c646bdcec3:a30913924d4053753b6e2add77f8a26f".getBytes("UTF-8"));
+        header = new HTTPHeader("Authorization", "Basic " + encoding);
+        req.setHeader(header);
+        
+      
+        String data = "{\"Messages\":[{\"From\":{\"Email\":\"sonu.hooda@gmail.com\", \"Name\": \"Me\" }, \"To\"  :[{\"Email\": \"sonu.hooda@gmail.com\", \"Name\": \"Me\"}], \"Subject\": \"My first Mailjet Email\",    \"TextPart\": \"Greetings from Mailjet.\",\"HTMLPart\":\"Hi\"}]}";
+        req.setPayload(data.getBytes());
+        com.google.appengine.api.urlfetch.HTTPResponse res = fetcher.fetch(req);
+        response.getWriter().println(new String(res.getContent()));
+        
+
+		
 	}
 
 	
